da4ae547e78ea8c6a120019a83cac0c92d1e8977
Bogdan_Mykhailov <bogdan_mykhailov@icloud.com>
2023-11-30 12:31:34 +0200

    update favicon and error component

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import s from './Error.module.css'
+import alert from '../../assets/icons/alert.png'
 
 export const Error = () => {
   return (
-      <h2 className={s.error}>
-        Sorry, we were unable to retrieve the currency data due to an error.
-      </h2>
+     <div className={s.errorWrapper}>
+       <img className={s.alert} src={alert} alt="alert"/>
+       <h2 className={s.error}>
+         Sorry, we were unable to retrieve the currency data due to an error.
+       </h2>
+     </div>
   );
 };
